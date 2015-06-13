f582961dcb9722c76f6599ec810babe5307d5f0f
suiyujin <suiyujin04@gmail.com>
2015-06-14 03:03:03 +0900

    fix unique indexes

--- db/migrate/20150613165724_rename_tables.rb
+++ db/migrate/20150613165724_rename_tables.rb
@@ -3,7 +3,12 @@ class RenameTables < ActiveRecord::Migration
     rename_table :illness_users, :illness_monstars
     rename_table :food_users, :food_monstars
 
+    remove_index :food_monstars, [:food_id, :user_id]
+    remove_index :illness_monstars, [:illness_id, :user_id]
+
     rename_column :food_monstars, :user_id, :monstar_id
+    add_index :food_monstars, [:food_id, :monstar_id], unique: true
     rename_column :illness_monstars, :user_id, :monstar_id
+    add_index :illness_monstars, [:illness_id, :monstar_id], unique: true
   end
 end
