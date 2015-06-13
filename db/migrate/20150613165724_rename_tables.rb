e3b4615419c224b029fca263919739cdf028db0b
suiyujin <suiyujin04@gmail.com>
2015-06-14 03:10:23 +0900

    fix foreign key indexes

--- db/migrate/20150613165724_rename_tables.rb
+++ db/migrate/20150613165724_rename_tables.rb
@@ -4,11 +4,15 @@ class RenameTables < ActiveRecord::Migration
     rename_table :food_users, :food_monstars
 
     remove_index :food_monstars, [:food_id, :user_id]
+    remove_index :food_monstars, :user_id
     remove_index :illness_monstars, [:illness_id, :user_id]
+    remove_index :illness_monstars, :user_id
 
     rename_column :food_monstars, :user_id, :monstar_id
+    add_index :food_monstars, :monstar_id, foreign_key: true
     add_index :food_monstars, [:food_id, :monstar_id], unique: true
     rename_column :illness_monstars, :user_id, :monstar_id
+    add_index :illness_monstars, :monstar_id, foreign_key: true
     add_index :illness_monstars, [:illness_id, :monstar_id], unique: true
   end
 end
